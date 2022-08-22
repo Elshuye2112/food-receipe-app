3d3660548f00468c18a921c812b1936134f00409
Elshuye2112 <basie.elsh2112@gmail.com>
2022-08-22 18:45:38 +0300

    first commit

---
+++ express/query/signup_query.js
@@ -0,0 +1,13 @@
+const signup_HASURA_OPERATION = `
+mutation signup($fname:String!,$lname:String!,$password:String! , $email:String!) {
+  insert_Accounts_one(object: {fname: $fname, lname: $lname, password: $password, email: $email}) {
+    id
+    email
+    fname
+    lname
+    password
+      }
+}
+`;
+
+module.exports = signup_HASURA_OPERATION;
